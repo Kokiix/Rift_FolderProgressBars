de544dc95530c41fe485194d7533bd7871c8ce18
kenneth <kokiidoki@gmail.com>
2026-06-11 15:27:28 -0400

    detect folder obj creation

--- FolderProgress/Patches/DrawProgressBar.cs
+++ FolderProgress/Patches/DrawProgressBar.cs
@@ -1,7 +1,15 @@
+
+using HarmonyLib;
+using Shared.TrackData;
+using Shared.TrackSelection;
+using UnityEngine;
+
+[HarmonyPatch(typeof(BaseTrackSelectionOptionGroup), "InitializeTrackOption")]
 static class DrawProgressBar
 {
-    static void Postfix()
+    static void Postfix(ITrackMetadata newTrackMetadata)
     {
-
+        if (newTrackMetadata is FolderTrackMetadata folderMeta)
+            Debug.LogError("folder object created: " + folderMeta.TrackName);
     }
 }
--- FolderProgress/Plugin.cs
+++ FolderProgress/Plugin.cs
@@ -13,11 +13,11 @@ using UnityEngine;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 class FolderPPlugin : BaseUnityPlugin
 {
-    internal static Harmony harmony;
+    Harmony harmony;
 
     void Awake()
     {
-        harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID);
+        harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
         harmony.PatchAll();
     }
     void OnDestroy()
