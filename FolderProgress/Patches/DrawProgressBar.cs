64ea192390d00617ecb49b5ecad66d1ca8b51815
kenneth <kokiidoki@gmail.com>
2026-06-11 16:21:21 -0400

    create progress bar obj

--- FolderProgress/Patches/DrawProgressBar.cs
+++ FolderProgress/Patches/DrawProgressBar.cs
@@ -5,11 +5,44 @@ using Shared.TrackSelection;
 using UnityEngine;
 
 [HarmonyPatch(typeof(BaseTrackSelectionOptionGroup), "InitializeTrackOption")]
-static class DrawProgressBar
+static class ToggleProgressBar
 {
-    static void Postfix(ITrackMetadata newTrackMetadata)
+    static GameObject ProgressBar;
+
+    static void Postfix(BaseTrackSelectionOptionGroup __instance, int optionIndex, ITrackMetadata newTrackMetadata)
     {
-        if (newTrackMetadata is FolderTrackMetadata folderMeta)
-            Debug.LogError("folder object created: " + folderMeta.TrackName);
+        if (newTrackMetadata is FolderTrackMetadata)
+        {
+            // Folder obj path is Canvas/ScreenContainer/BounceContainer/Content/InfiniteTrackList/ScrollMask/InfiniteScrollTrackArea/TrackOption_Folder(Clone)/
+            var container = __instance._options[optionIndex].transform.Find("BounceContainer");
+            var existingBar = container.Find("ProgressBar(Clone)");
+            if (existingBar)
+            {
+                // Debug
+                Object.Destroy(existingBar.gameObject);
+
+                // existingBar.gameObject.SetActive(true);
+            }
+            // else
+            // {
+            if (!ProgressBar)
+                CreateProgressBar(container);
+            AttachProgressBar(container);
+            // }
+        }
+    }
+
+    static void CreateProgressBar(Transform trackOption)
+    {
+        ProgressBar = Object.Instantiate(trackOption.Find("LetterGradeDashed").gameObject);
+        ProgressBar.name = "ProgressBar";
+        ProgressBar.transform.localPosition = new Vector3(-500, -30, 0);
+    }
+
+    static void AttachProgressBar(Transform trackOption)
+    {
+        var bar = Object.Instantiate(ProgressBar);
+        bar.transform.SetParent(trackOption, worldPositionStays: false);
+        bar.SetActive(true);
     }
 }
