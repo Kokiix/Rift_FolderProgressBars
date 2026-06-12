6355442d41ae144249c13be5f848d9736931d7d1
kenneth <kokiidoki@gmail.com>
2026-06-11 21:10:21 -0400

    handle difficulty change

--- FolderProgress/Patches/DrawProgressBar.cs
+++ FolderProgress/Patches/DrawProgressBar.cs
@@ -18,55 +18,75 @@ static class ApplyBarToNewOption
     {
         if (newTrackMetadata is FolderTrackMetadata)
         {
-            var container = __instance._options[optionIndex].transform.Find("BounceContainer/Background");
-            var existingBar = container.Find("ProgressBar(Clone)");
+            ProgressBar.ApplyBarToFolder(__instance, optionIndex, folderName);
+        }
+    }
+}
 
-            GameObject barInstance = null;
-            if (existingBar)
-            {
-                // Debug
-                UnityEngine.Object.Destroy(existingBar.gameObject);
-
-                // existingBar.gameObject.SetActive(true);
-            }
-            // else
-            // {
-            if (!ProgressBar.Template)
-                ProgressBar.InitTemplate(container.parent.Find("LetterGradeDashed"));
-            barInstance = UnityEngine.Object.Instantiate(ProgressBar.Template);
-            barInstance.transform.SetParent(container, worldPositionStays: false);
-            barInstance.SetActive(true);
-
-            // }
-
-            int totalTracksInFolder = 0;
-            double FCedTracks = 0;
-
-            // Loop through all tracks bc idk how to get idx of newly created folder..
-            var trackIndex =
-            Enumerable.Range(0, __instance._trackMetaData.Count)
-            .First(i => __instance._trackMetaData[i].TrackName == folderName);
-
-            // Go through tracks under folder and count FCs
-            while (++trackIndex < __instance._trackMetaData.Count &&
-            __instance.GetFolderIndexForTrack(trackIndex) != -1 &&
-            __instance._trackMetaData[__instance.GetFolderIndexForTrack(trackIndex)].TrackName == folderName)
-            {
-                var track = __instance._trackMetaData[trackIndex];
-
-                totalTracksInFolder++;
-                if (PlayerDataUtil.GetIsFullComboForDifficulty(track.LevelId, __instance._selectedDifficulty, __instance._options[0].GetStatMode()))
-                    FCedTracks++;
-            }
-
-            ProgressBar.SetPercentage(barInstance, Math.Round(FCedTracks / totalTracksInFolder, 2));
+[HarmonyPatch(typeof(BaseTrackSelectionOptionGroup), "HandleDifficultyChanged")]
+static class ApplyBarToAllOptions
+{
+    static void Postfix(BaseTrackSelectionOptionGroup __instance)
+    {
+        for (int i = 0; i < __instance._options.Count; i++)
+        {
+            var meta = __instance._options[i].Metadata;
+            if (meta is FolderTrackMetadata)
+                ProgressBar.ApplyBarToFolder(__instance, i, meta.TrackName);
         }
     }
 }
 
 static class ProgressBar
 {
-    internal static GameObject Template;
+    static GameObject Template;
+
+    internal static void ApplyBarToFolder(BaseTrackSelectionOptionGroup __instance, int optionIndex, string folderName)
+    {
+        var container = __instance._options[optionIndex].transform.Find("BounceContainer/Background");
+        var existingBar = container.Find("ProgressBar(Clone)");
+
+        GameObject barInstance = null;
+        if (existingBar)
+        {
+            // Debug
+            UnityEngine.Object.Destroy(existingBar.gameObject);
+
+            // existingBar.gameObject.SetActive(true);
+        }
+        // else
+        // {
+        if (!ProgressBar.Template)
+            ProgressBar.InitTemplate(container.parent.Find("LetterGradeDashed"));
+        barInstance = UnityEngine.Object.Instantiate(ProgressBar.Template);
+        barInstance.transform.SetParent(container, worldPositionStays: false);
+        barInstance.SetActive(true);
+
+        // }
+
+        int totalTracksInFolder = 0;
+        double FCedTracks = 0;
+
+        // Loop through all tracks bc idk how to get idx of newly created folder..
+        var trackIndex =
+        Enumerable.Range(0, __instance._trackMetaData.Count)
+        .First(i => __instance._trackMetaData[i].TrackName == folderName);
+
+        // Go through tracks under folder and count FCs
+        while (++trackIndex < __instance._trackMetaData.Count &&
+        __instance.GetFolderIndexForTrack(trackIndex) != -1 &&
+        __instance._trackMetaData[__instance.GetFolderIndexForTrack(trackIndex)].TrackName == folderName)
+        {
+            var track = __instance._trackMetaData[trackIndex];
+
+            totalTracksInFolder++;
+            if (PlayerDataUtil.GetIsFullComboForDifficulty(track.LevelId, __instance._selectedDifficulty, __instance._options[0].GetStatMode()))
+                FCedTracks++;
+        }
+
+        ProgressBar.SetPercentage(barInstance, Math.Round(FCedTracks / totalTracksInFolder, 2));
+
+    }
 
     const int MaxBarWidth = 300;
     const int MaxBarHeight = 15;
@@ -76,7 +96,7 @@ static class ProgressBar
     static readonly Vector3 BarPosition = new Vector3(250, -40, 0);
     static readonly Vector3 NumberPosition = new Vector3(50, -40, 0);
 
-    internal static void InitTemplate(Transform textTemplate)
+    static void InitTemplate(Transform textTemplate)
     {
         // TODO: make color change on select (likely something to do with TrackSelectionOptionColorAnimator)
         Template = new GameObject("ProgressBar", typeof(RectTransform));
@@ -100,7 +120,7 @@ static class ProgressBar
         number.SetActive(true);
     }
 
-    internal static void SetPercentage(GameObject bar, double percent)
+    static void SetPercentage(GameObject bar, double percent)
     {
         var innerBar = bar.transform.Find("outerBar/innerBar");
         var innerBarWidth = MaxBarWidth * (float)percent;
